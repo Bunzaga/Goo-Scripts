bd570fb7de408aae75379ae2f2e4f6aa28d37be9
Brett Unzaga <Bunzaga@gmail.com>
2014-03-26 11:40:20 -0600

    Update Game.js

--- Game.js
+++ Game.js
@@ -1,15 +1,14 @@
 if(null == NodeList){
   document.body.write('<script type = "text/javascript" src = ""></script>');
 }
-var Game = function(window, undefined){
-  var _instance;
-  return function(){
-    if(!_instance){
-      _instance = this;
-      var listeners = {};
-      this.bind = function(){};
-      this.unbind = function(){};
-      this.raise = function(){};
+window.em = null;
+var EventManager = function() {
+    if (em) {
+    } else {
+        this.bind = function(){};
+        this.unbind = function(){};
+        this.raise = function(){};
+        window.em = this;
     }
-  }
-}();
+    return window.em;
+};
