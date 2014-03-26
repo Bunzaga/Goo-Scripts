e80c59e6c8d58bb1fa40e3db855b0e6316051240
Brett Unzaga <Bunzaga@gmail.com>
2014-03-26 13:32:47 -0600

    Update Movement.js

--- Movement.js
+++ Movement.js
@@ -1,24 +1,28 @@
 /* Implement this method to do initializing */
-var _args = null;
-var _ctx = null;
+var _args;
+var _ctx;
 var state = 0;
 var setup = function(args, ctx, goo) {
 	_args = args;
-	_args.rot = 0;
 	_ctx = ctx;
+	args.rot0 = 0;
+	args.rot1 = 0;
+	args.pivot0 = ctx.world.by.name("Pivot0").first();
+	args.pivot1 = ctx.world.by.name("Pivot1").first();
+	args.cam = ctx.world.by.name("ViewCam").first();
 };
 
 /* Implement this method to do cleanup on script stop and delete */
-var cleanup = function(args, ctx, goo) {
-	_args = null;
-	_ctx = null;
-};
+var cleanup = function(args, ctx, goo) {};
 
 function mouseTest(){
 	console.log(_ctx.world.MouseInput.delta.x);
-	_args.rot += _ctx.world.MouseInput.delta.x * 0.01;
-	_ctx.entity.transformComponent.transform.rotation.fromAngles(0, _args.rot, 0);
-	_ctx.entity.transformComponent.setUpdated();
+	_args.rot0 -= _ctx.world.MouseInput.delta.x * _args.xAxis;
+	_args.rot1 += _ctx.world.MouseInput.delta.y * _args.yAxis;
+	_args.pivot0.transformComponent.transform.rotation.fromAngles(0, _args.rot0, 0);
+	_args.pivot0.transformComponent.setUpdated();
+	_args.pivot1.transformComponent.transform.rotation.fromAngles(_args.rot1, 0, 0);
+	_args.pivot1.transformComponent.setUpdated();
 }
 
 /**
@@ -45,7 +49,7 @@ var update = function(args, ctx, goo) {
 	switch(state){
 		case 0:
 		if(ctx.world.em){
-			ctx.world.em.bind("MouseMove", _args, mouseTest);
+			ctx.world.em.bind("MouseMove", null, mouseTest);
 			state = 1;
 		}
 		break;
@@ -72,4 +76,7 @@ var update = function(args, ctx, goo) {
  *  exponential?: boolean, // Used together with slider
  * }
  */
-var parameters = [];
+var parameters = [
+	{key:'xAxis', type:'float', default:0.001, decimals:3},
+	{key:'yAxis', type:'float', default:0.001, decimals:3}
+];
