3027f6dfd0b7a30d9d1eb453f0d71a58d88a3bea
Brett Unzaga <Bunzaga@gmail.com>
2014-04-14 11:26:44 -0600

    Update KeyInput.js

--- KeyInput.js
+++ KeyInput.js
@@ -21,8 +21,8 @@ define([], function(){
 		callbacks[keyCode](false);
 	}
 
-    document.body.addEventListener('keyup', keyUp, false);
-    document.body.addEventListener('keydown', keyDown, false);
+	document.documentElement.addEventListener("keyup", keyUp, false);
+	document.documentElement.addEventListener("keydown", keyDown, false);
 	
 	var stringToCode = {"backspace":8,"tab":9,"enter":13,"shift":16,"ctrl":17,"alt":18,"pause":19,"caps":20,"esc":27,"space":32,"page up":33,"page down":34,"end":35,"home":36,
 	"left":37,"up":38,"right":39,"down":40,"insert":45,"delete":46,"0":48,"1":49,"2":50,"3":51,"4":52,"5":53,"6":54,"7":55,"8":56,"9":57,
