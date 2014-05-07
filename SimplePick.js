875ebe9b7a6862c621c9605e4b83e7e942fd916f
Brett Unzaga <Bunzaga@gmail.com>
2014-05-07 07:07:27 -0600

    Update SimplePick.js

--- SimplePick.js
+++ SimplePick.js
@@ -1,51 +1,95 @@
 function SimplePick(args, ctx, goo){
-	if(undefined === ctx.picking){
-		var picking = new goo.PickingSystem({pickLogic: new goo.PrimitivePickLogic()});
-		var v1 = new goo.Vector3();
-		var v2 = new goo.Vector3();
-		var cross = new goo.Vector3();
+	var picking = new goo.PickingSystem({pickLogic: new goo.PrimitivePickLogic()});
+	var v1 = new goo.Vector3();
+	var v2 = new goo.Vector3();
+	var cross = new goo.Vector3();
 
 
-		this.ray = new goo.Ray();
-		// extend our 'primitive pick logic' with something a little less primitive
-		var hitSort = function(a, b){
-			return a.distance-b.distance;
-		}
-		picking.onPick = function(result){
-			var hit = null;
-			if(null !== result){
-				console.log("result:");
-				console.log(result);
-				if(result.length > 0){
-					var distance = typeof picking.pickRay.distance !== 'undefined' ? picking.pickRay.distance : Infinity;
-					var rayDir = picking.pickRay.direction;
-					if(picking.mask){
-						if(picking.all){ // add all non-culled faces to hit array
-							for(var i = 0, ilen = result.length; i < ilen; i++){
-								if((result[i].entity.hitMask & picking.mask) !== 0){
-									for(var j = 0, jlen = result[i].intersection.distances.length; j < jlen; j++){
-										var entDistance = result[i].intersection.distances[j];
-										if(entDistance < distance){
-											// create two CCW 'edge vectors' based on the points of the face hit
-											var v0 = result[i].intersection.vertices[j][0];
-											var v1 = result[i].intersection.vertices[j][1];
-											var v2 = result[i].intersection.vertices[j][2];
-												
-											goo.Vector3.subv(v0, v1, v1);
-											goo.Vector3.subv(v0, v2, v2);
-											//ctx.v1.normalize();
-											//ctx.v2.normalize();
+	this.ray = new goo.Ray();
+	// extend our 'primitive pick logic' with something a little less primitive
+	var hitSort = function(a, b){
+		return a.distance-b.distance;
+	}
+	picking.onPick = function(result){
+		var hit = null;
+		if(null !== result){
+			console.log("result:");
+			console.log(result);
+			if(result.length > 0){
+				var distance = typeof picking.pickRay.distance !== 'undefined' ? picking.pickRay.distance : Infinity;
+				var rayDir = picking.pickRay.direction;
+				if(picking.mask){
+					if(picking.all){ // add all non-culled faces to hit array
+						for(var i = 0, ilen = result.length; i < ilen; i++){
+							if((result[i].entity.hitMask & picking.mask) !== 0){
+								for(var j = 0, jlen = result[i].intersection.distances.length; j < jlen; j++){
+									var entDistance = result[i].intersection.distances[j];
+									if(entDistance < distance){
+										// create two CCW 'edge vectors' based on the points of the face hit
+										var v0 = result[i].intersection.vertices[j][0];
+										var v1 = result[i].intersection.vertices[j][1];
+										var v2 = result[i].intersection.vertices[j][2];
 											
-											// use the cross product of the face edges to get the 'normal'
-											goo.Vector3.cross(v1, v2, cross);
-											cross.normalize();
-											
-											var dot = goo.Vector3.dot(cross, rayDir);
-											if(dot < 0){
-												if(null === hit){
-													hit = [];
-												}
-												hit.push({
+										goo.Vector3.subv(v0, v1, v1);
+										goo.Vector3.subv(v0, v2, v2);
+										//ctx.v1.normalize();
+										//ctx.v2.normalize();
+										
+										// use the cross product of the face edges to get the 'normal'
+										goo.Vector3.cross(v1, v2, cross);
+										cross.normalize();
+										
+										var dot = goo.Vector3.dot(cross, rayDir);
+										if(dot < 0){
+											if(null === hit){
+												hit = [];
+											}
+											hit.push({
+												entity: result[i].entity,
+												point: new goo.Vector3().copy(result[i].intersection.points[j]),
+												normal: new goo.Vector3().copy(cross),
+												vertices:[
+													new goo.Vector3().copy(v0),
+													new goo.Vector3().copy(v1),
+													new goo.Vector3().copy(v2)],
+												distance: entDistance
+											});
+										}
+									}
+								}
+							}
+						}
+						if(hit && hit.length > 1){
+							hit.sort(hitSort);
+						}
+					}
+					else{ // only return the closest non-culled face
+						for(i = 0, ilen = result.length; i < ilen; i++){
+							if((result[i].entity.hitMask & picking.mask) !== 0){
+								for(j = 0, jlen = result[i].intersection.distances.length; j < jlen; j++){
+									entDistance = result[i].intersection.distances[j];
+									if(entDistance < distance){
+										// create two CCW 'edge vectors' based on the points of the face hit
+										v0 = result[i].intersection.vertices[j][0];
+										v1 = result[i].intersection.vertices[j][1];
+										v2 = result[i].intersection.vertices[j][2];
+										
+										goo.Vector3.subv(v0, v1, v1);
+										goo.Vector3.subv(v0, v2, v2);
+										//ctx.v1.normalize();
+										//ctx.v2.normalize();
+										
+										// use the cross product of the face edges to get the 'normal'
+										goo.Vector3.cross(v1, v2, cross);
+										cross.normalize();
+										
+										dot = goo.Vector3.dot(cross, rayDir);
+										// dot the normal with the ray.dir to see if it is backfacing or not
+										// if not backfacing
+										if(dot < 0){
+											distance = result[i].intersection.distances[j];
+											if(null === hit){
+												hit = {
 													entity: result[i].entity,
 													point: new goo.Vector3().copy(result[i].intersection.points[j]),
 													normal: new goo.Vector3().copy(cross),
@@ -54,86 +98,40 @@ function SimplePick(args, ctx, goo){
 														new goo.Vector3().copy(v1),
 														new goo.Vector3().copy(v2)],
 													distance: entDistance
-												});
+												}
 											}
-										}
-									}
-								}
-							}
-							if(hit && hit.length > 1){
-								hit.sort(hitSort);
-							}
-						}
-						else{ // only return the closest non-culled face
-							for(i = 0, ilen = result.length; i < ilen; i++){
-								if((result[i].entity.hitMask & picking.mask) !== 0){
-									for(j = 0, jlen = result[i].intersection.distances.length; j < jlen; j++){
-										entDistance = result[i].intersection.distances[j];
-										if(entDistance < distance){
-											// create two CCW 'edge vectors' based on the points of the face hit
-											v0 = result[i].intersection.vertices[j][0];
-											v1 = result[i].intersection.vertices[j][1];
-											v2 = result[i].intersection.vertices[j][2];
-											
-											goo.Vector3.subv(v0, v1, v1);
-											goo.Vector3.subv(v0, v2, v2);
-											//ctx.v1.normalize();
-											//ctx.v2.normalize();
-											
-											// use the cross product of the face edges to get the 'normal'
-											goo.Vector3.cross(v1, v2, cross);
-											cross.normalize();
-											
-											dot = goo.Vector3.dot(cross, rayDir);
-											// dot the normal with the ray.dir to see if it is backfacing or not
-											// if not backfacing
-											if(dot < 0){
-												distance = result[i].intersection.distances[j];
-												if(null === hit){
-													hit = {
-														entity: result[i].entity,
-														point: new goo.Vector3().copy(result[i].intersection.points[j]),
-														normal: new goo.Vector3().copy(cross),
-														vertices:[
-															new goo.Vector3().copy(v0),
-															new goo.Vector3().copy(v1),
-															new goo.Vector3().copy(v2)],
-														distance: entDistance
-													}
-												}
-												else{
-													hit.entity = result[i].entity;
-													hit.point.copy(result[i].intersection.points[j]);
-													hit.normal.copy(cross);
-													hit.vertices[0].copy(v0);
-													hit.vertices[1].copy(v1);
-													hit.vertices[2].copy(v2);
-													hit.distance = entDistance;
-												}
+											else{
+												hit.entity = result[i].entity;
+												hit.point.copy(result[i].intersection.points[j]);
+												hit.normal.copy(cross);
+												hit.vertices[0].copy(v0);
+												hit.vertices[1].copy(v1);
+												hit.vertices[2].copy(v2);
+												hit.distance = entDistance;
 											}
 										}
 									}
 								}
 							}
 						}
-					}
-					else{
-						console.error("SimplePick.castRay: You must pass in a mask as the second parameter.");
 					}
 				}
+				else{
+					console.error("SimplePick.castRay: You must pass in a mask as the second parameter.");
+				}
 			}
-			picking.hit = hit;
 		}
-		
-		this.added = function(ent){picking.added(ent);}
-		
-		this.castRay = function(ray, mask, all){
-			picking.pickRay = ray;
-			picking.mask = mask;
-			picking.all = all;
-			picking._process();
-			return picking.hit;
-		}
+		picking.hit = hit;
+	}
+	
+	this.added = function(ent){picking.added(ent);}
+	
+	this.castRay = function(ray, mask, all){
+		picking.pickRay = ray;
+		picking.mask = mask;
+		picking.all = all;
+		picking._process();
+		return picking.hit;
 	}
     
 	if(undefined === ctx.world.getSystem("PickingSystem")){
