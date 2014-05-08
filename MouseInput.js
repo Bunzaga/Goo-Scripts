89d5a73cccdab265db915c65d581e814af10c4e5
Brett Unzaga <Bunzaga@gmail.com>
2014-05-08 14:35:20 -0600

    Update MouseInput.js

--- MouseInput.js
+++ MouseInput.js
@@ -1,55 +1,59 @@
-/* Implement this method to do initializing */
-var setup = function(args, ctx, goo) {
-	ctx.buttons = {};
-	ctx.stringToCode = {"left":1, "right":2, "middle":4, "wheel":8, "move":16};
-	ctx.offsetLeft = ctx.domElement.getBoundingClientRect().left;
-	ctx.offsetTop = ctx.domElement.getBoundingClientRect().top;
+function MouseInput (args, ctx, goo) {
+	var buttons = {};
+	var stringToCode = {"left":1, "right":2, "middle":4, "wheel":8, "move":16};
+	var offsetLeft = ctx.domElement.getBoundingClientRect().left;
+	var offsetTop = ctx.domElement.getBoundingClientRect().top;
+	var _this = this;
 
-	var MouseInput = {};
-	MouseInput.movement = new goo.Vector2();
-	MouseInput.delta = new goo.Vector2();
-	MouseInput.old = new goo.Vector2();
-	MouseInput.position = new goo.Vector2();
-	MouseInput.getButton = function(btnCode){
-		var btn = typeof btnCode === 'number' ? btnCode : ctx.stringToCode[btnCode];
-		return ctx.buttons[btn];
-	}
-	MouseInput.bind = function(btnCode, callback){
-		var btn = typeof btnCode === 'number' ? btnCode : ctx.stringToCode[btnCode];
-		ctx.buttons[btn] = false;
+	this.movement = new goo.Vector2();
+	this.delta = new goo.Vector2();
+	this.old = new goo.Vector2();
+	this.position = new goo.Vector2();
+	
+	this.getButton = function(btnCode){
+		var btn = typeof btnCode === 'number' ? btnCode : stringToCode[btnCode];
+		return buttons[btn];
+	};
+	
+	this.bind = function(btnCode, callback){
+		var btn = typeof btnCode === 'number' ? btnCode : stringToCode[btnCode];
+		buttons[btn] = false;
 		if(callback){
 			if(typeof callback === 'function'){
-				console.log("MouseInput.bind:ctx.callbacks["+btnCode+"]");
 				goo.SystemBus.addListener("MouseInput"+btn, callback);
 			}
 		}
-		return MouseInput;
-	}
-	MouseInput.unbind = function(btnCode, callback){
+		return this;
+	};
+	
+	this.unbind = function(btnCode, callback){
 		if(null === callback){
 			console.warn("MouseInput.unbind: You should pass in the callback to remove, did you mean 'MouseInput.unbindAll ?");
-			MouseInput.unbindAll(btnCode);
-			return MouseInput;
+			this.unbindAll(btnCode);
+			return this;
 		}
-		var btn = typeof btnCode === 'number' ? btnCode : ctx.stringToCode[btnCode];
-		if(undefined !== ctx.buttons[btn]){
+		var btn = typeof btnCode === 'number' ? btnCode : stringToCode[btnCode];
+		if(undefined !== buttons[btn]){
 			if(typeof callback === 'function'){
 				goo.SystemBus.removeListener("MouseInput"+btn, callback);
 			}
 		}
-		return MouseInput;
-	}
-	MouseInput.unbindAll = function(btnCode){
-		var btn = typeof btnCode === 'number' ? btnCode : ctx.stringToCode[btnCode];
+		return this;
+	};
+	
+	this.unbindAll = function(btnCode){
+		var btn = typeof btnCode === 'number' ? btnCode : stringToCode[btnCode];
 		goo.SystemBus.removeAllOnChannel("MouseInput"+btn);
-		return MouseInput;
-	}
-	ctx.mouseWheel = function(e){
+		return this;
+	};
+	
+	function mouseWheel(e){
 		e = e || window.event;
 		var wheelDelta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
 		goo.SystemBus.emit("MouseInput8", wheelDelta);
-	};
-	ctx.mouseDown = function(e){
+	}
+	
+	function mouseDown(e){
 		var btn = 0;
 		if(null === e.which){
 			btn = e.button;
@@ -67,11 +71,11 @@ var setup = function(args, ctx, goo) {
 					break;
 			}
 		}
-		if(true === ctx.buttons[btn]){return;}
-		ctx.buttons[btn] = true;
+		if(true === buttons[btn]){return;}
+		buttons[btn] = true;
 		goo.SystemBus.emit("MouseInput"+btn, true);
-	};
-	ctx.mouseUp = function(e){
+	}
+	function mouseUp(e){
 		//updateMousePos(e);
 		var btn = 0;
 		if(null === e.which){
@@ -90,15 +94,15 @@ var setup = function(args, ctx, goo) {
 					break;
 			}
 		}
-		if(false === ctx.buttons[btn]){return;}
-		ctx.buttons[btn] = false;
+		if(false === buttons[btn]){return;}
+		buttons[btn] = false;
 		goo.SystemBus.emit("MouseInput"+btn, false);
-	};
-	ctx.mouseMove = function(e){
-		ctx.updateMousePos(e);
+	}
+	function mouseMove(e){
+		updateMousePos(e);
 		goo.SystemBus.emit("MouseInput16", false);
 	}
-	ctx.updateMousePos = function(e){
+	function updateMousePos(e){
 		e = e || window.event;
 		if (e && e.preventDefault) {e.preventDefault();}
 		if (e && e.stopPropagation) {e.stopPropagation();}
@@ -108,37 +112,34 @@ var setup = function(args, ctx, goo) {
 
 		var newY = e.pageY ? e.pageY : e.clientY + (document.documentElement.scrollTop) ||
 			(document.body.scrollTop - document.documentElement.scrollTop);
-
-		newX -= (ctx.offsetLeft + ctx.domElement.offsetLeft);
-		newY -= (ctx.offsetTop + ctx.domElement.offsetTop);
-		MouseInput.movement.x = e.movementX;
-		MouseInput.movement.y = e.movementY;
-		MouseInput.delta.x = newX - MouseInput.position.x;
-		MouseInput.delta.y = newY - MouseInput.position.y;
-		MouseInput.old.x = MouseInput.position.x;
-		MouseInput.old.y = MouseInput.position.y;
-		MouseInput.position.x = newX;
-		MouseInput.position.y = newY;
-	};
-
-	document.documentElement.addEventListener('mousedown', ctx.mouseDown, false);
-	document.documentElement.addEventListener('mouseup', ctx.mouseUp, false);
-	document.documentElement.addEventListener('mousemove', ctx.mouseMove, false);
-	document.documentElement.addEventListener("mousewheel", ctx.mouseWheel, false);
-	document.documentElement.addEventListener("DOMMouseScroll", ctx.mouseWheel, false); // Firefox
-
-	ctx.worldData.MouseInput = MouseInput;
-};
-
-/* Implement this method to do cleanup on script stop and delete */
-var cleanup = function(args, ctx, goo) {
-	for(var i in ctx.buttons){
-		goo.SystemBus.removeAllOnChannel("MouseInput"+i);
+		newX -= (offsetLeft + ctx.domElement.offsetLeft);
+		newY -= (offsetTop + ctx.domElement.offsetTop);
+		_this.movement.x = e.movementX;
+		_this.movement.y = e.movementY;
+		_this.delta.x = newX - _this.position.x;
+		_this.delta.y = newY - _this.position.y;
+		_this.old.x = _this.position.x;
+		_this.old.y = _this.position.y;
+		_this.position.x = newX;
+		_this.position.y = newY;
 	}
-	document.documentElement.removeEventListener('mousemove', ctx.mouseMove, false);
-	document.documentElement.removeEventListener('mousedown', ctx.mouseDown, false);
-	document.documentElement.removeEventListener('mouseup', ctx.mouseUp, false);
-	document.documentElement.removeEventListener("mousewheel", ctx.mouseWheel, false);
-	document.documentElement.removeEventListener("DOMMouseScroll", ctx.mouseWheel, false); // Firefox
-	delete ctx.worldData.MouseInput;
-};
+
+	document.documentElement.addEventListener('mousedown', mouseDown, false);
+	document.documentElement.addEventListener('mouseup', mouseUp, false);
+	document.documentElement.addEventListener('mousemove', mouseMove, false);
+	document.documentElement.addEventListener("mousewheel", mouseWheel, false);
+	document.documentElement.addEventListener("DOMMouseScroll", mouseWheel, false); // Firefox
+
+	/* Implement this method to do cleanup on script stop and delete */
+	this.cleanup = function(args, ctx, goo) {
+		for(var i in buttons){
+			goo.SystemBus.removeAllOnChannel("MouseInput"+i);
+		}
+		document.documentElement.removeEventListener('mousemove', mouseMove, false);
+		document.documentElement.removeEventListener('mousedown', mouseDown, false);
+		document.documentElement.removeEventListener('mouseup', mouseUp, false);
+		document.documentElement.removeEventListener("mousewheel", mouseWheel, false);
+		document.documentElement.removeEventListener("DOMMouseScroll", mouseWheel, false); // Firefox
+		delete ctx.worldData.MouseInput;
+	}
+}
