27ca5a53a22197a7d2de7756cb975c3688c7fd44
Brett Unzaga <Bunzaga@gmail.com>
2014-05-22 04:59:41 -0600

    Update AmmoSystem.js

--- AmmoSystem.js
+++ AmmoSystem.js
@@ -3,19 +3,8 @@
   function AmmoSystem(args, ctx, goo){
     this.ctx = ctx;
     this.goo = goo;
-    this.setup(args, ctx, goo);
   }
-  
-  AmmoSystem.prototype.setup = function(args, ctx, goo){
-    
-  }
-  AmmoSystem.prototype.update = function(args, ctx, goo){
-    
-  }
-  AmmoSystem.prototype.cleanup = function(args, ctx, goo){
-    
-  }
-  
+
   AmmoSystem.parameters = [
     {key:'gravity', type:'vec3', default:[0.0, -9.8, 0.0]},
     {key:'maxSubSteps', type:'int', default:5},
