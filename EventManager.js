b5c4e9d16cef78df27681c1e337b0e828b29e197
Brett Unzaga <Bunzaga@gmail.com>
2014-05-21 22:24:47 -0600

    Update EventManager.js

--- EventManager.js
+++ EventManager.js
@@ -3,27 +3,52 @@
   var eventList = {};
   EventManager.setup = function(args, ctx, goo){
 
-    EventManager.bind = function(event, callback, priority){
-      if(undefined === eventList[event]){
-        eventList[event] = {first:null, last:null};
+    EventManager.bind = function(e, callback, priority){
+      if(undefined === eventList[e]){
+        eventList[e] = {first:null, last:null};
       }
       var node = {previous:null, next:null, callback:callback};
       if(undefined === priority){
-        addFirst(eventList[event], node);
+        //addFirst(eventList[e], node);
       }
       else{
         node.priority = priority;
-        addSorted(eventList[event], node);
+        //addSorted(eventList[e], node);
       }
       return EventManager;
     };
-    EventManager.unbind = function(event, callback){
+    EventManager.unbind = function(e, callback){
+      if(undefined === eventList[e]){return;}
+      var node = eventList[e].first;
+      while(node != null){
+      	if(node.callback === callback){
+      		break;
+      	}
+      	node = node.next;
+      }
+      if(node !== null){
+      	if(eventList[e].first === node){
+      		eventList[e].first = eventList[e].first.next;
+      	}
+      	if(eventList[e].last === node){
+      		eventList[e].last = eventList[e].last.previous;
+      	}
+      	if(node.previous !== null){
+      		node.previous.next = node.next;
+      	}
+      	if(node.next !== null ){
+      		node.next.previous = node.previous;
+      	}
+      }
+      if(null === eventList[e].first){
+      	delete eventList[e];
+      }
       return EventManager;
     };
-    EventManger.unbindAll = function(event){
+    EventManger.unbindAll = function(e){
       return EventManager;
     };
-    EventManager.emit = function(event){
+    EventManager.emit = function(){
       return EventManager;
     };
     EventManager.cleanup = function(){
