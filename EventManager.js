4ae958b7eb2e0d43ecfaffbcf354e0594323d11c
Brett Unzaga <Bunzaga@gmail.com>
2014-05-21 22:12:17 -0600

    Update EventManager.js

--- EventManager.js
+++ EventManager.js
@@ -2,17 +2,18 @@
   var EventManager = {};
   var eventList = {};
   EventManager.setup = function(args, ctx, goo){
+
     EventManager.bind = function(event, callback, priority){
-      
+      return EventManager;
     };
     EventManager.unbind = function(event, callback){
-      
+      return EventManager;
     };
     EventManger.unbindAll = function(event){
-      
+      return EventManager;
     };
     EventManager.emit = function(event){
-      
+      return EventManager;
     };
     EventManager.cleanup = function(){
       delete EventManager.bind;
