71829bda8e5a3431ade2ef7b0f3e7b54da1e7078
Brett Unzaga <Bunzaga@gmail.com>
2014-05-21 22:02:50 -0600

    Update EventManager.js

--- EventManager.js
+++ EventManager.js
@@ -1,6 +1,17 @@
 (function(window, document){
   var EventManager = {};
-  
+  EventManager.bind = function(){
+    
+  }
+  EventManager.unbind = function(){
+    
+  }
+  EventManger.unbindAll = function(){
+    
+  }
+  EventManager.emit = function(){
+    
+  }
   var global = global || window;
   global.EventManager = EventManager;
 }(window, document, undefined));
