36675052493f6cc9067ba4cdb5fd00801c15cd78
Brett Unzaga <Bunzaga@gmail.com>
2014-05-22 21:09:38 -0600

    Update AmmoUtil.js

--- AmmoUtil.js
+++ AmmoUtil.js
@@ -1,12 +1,25 @@
 (function(window, document){
   var AmmoUtil = {};
   var pvec;
+  var ptrans;
+  var pquat;
   
   AmmoUtil.setLinearVelocity = function(body, vec3){
   	pvec = pvec || new Ammo.btVector3();
+  	
   	pvec.setValue(vec3.x, vec3.y, vec3.z);
 	body.setLinearVelocity(pvec);
   };
+  AmmoUtil.setRotation = function(body, quat){
+ 	ptrans = ptrans || new Ammo.btTransform();
+ 	pquat = pquat || new Ammo.btQuaternion();
+ 	
+	body.getMotionState().getWorldTransform(ptrans);
+	pquat = ptrans.getRotation();
+	pquat.setValue(quat.x, quat.y, quat.z, quat.w);
+	ptrans.setRotation(pquat);
+	body.getMotionState().setWorldTransform(ptrans);
+  }
   var global = global || window;
   global.AmmoUtil = AmmoUtil;
 }(window, document, undefined));
