4ad4547825a98c32a370ae6f48ff1e2ef945e065
Brett Unzaga <Bunzaga@gmail.com>
2014-05-24 10:10:41 -0600

    Update bAmmoSystem.js

--- bAmmoSystem.js
+++ bAmmoSystem.js
@@ -3,7 +3,7 @@
   }
   
   bAmmoSystem.parameters = [
-  {key:'gravity', type:'vec3', default[0.0, -9.8, 0.0]}];
+  {key:'gravity', type:'vec3', default:[0.0, -9.8, 0.0]}];
   
   var global = global || window;
   global.bAmmoSystem = bAmmoSystem;
