03d6a05956c2bd84485ece0f2c6507964186cf57
Brett Unzaga <Bunzaga@gmail.com>
2014-05-24 02:46:01 -0600

    Update EventManager.js

--- EventManager.js
+++ EventManager.js
@@ -4,11 +4,13 @@
 
 	EventManager.bind = function(e, callback, priority){
 		if(undefined === eventList[e]){
-			eventList[e] = {first:null, last:null};
+			eventList[e] = new NodeList();
+			//eventList[e] = {first:null, last:null};
 		}
 		var node = {previous:null, next:null, callback:callback};
 		if(undefined === priority){
-			if(null === eventList[e].first){
+			eventList[e].addFirst(node);
+			/*if(null === eventList[e].first){
     				eventList[e].first = node;
     				eventList[e].last = node;
     				node.next = null;
@@ -18,11 +20,12 @@
     				node.next = eventList[e].first;
     				eventList[e].first.previous = node;
     				eventList[e].first = node;
-    			}
+    			}*/
 		}
 		else{
 			node.priority = priority;
-			if(null == eventList[e].first){
+			eventList[e].addSorted(node);
+			/*if(null == eventList[e].first){
 				eventList[e].first = node;
 				eventList[e].last = node;
 				node.next = null;
@@ -55,7 +58,7 @@
 	    				n.next.previous = node;
 	    				n.next = node;
 	    			}
-	    		}
+	    		}*/
 	      }
 	      return EventManager;
 	};
@@ -66,6 +69,7 @@
 			return EventManager;
 		}
 		if(undefined !== eventList[e]){
+			
 		        var node = eventList[e].first;
 		        while(node != null){
 		        	if(node.callback === callback){
@@ -74,7 +78,8 @@
 		        	node = node.next;
 		        }
 		        if(node !== null){
-		        	if(eventList[e].first === node){
+		        	eventList[e].remove(node);
+		        	/*if(eventList[e].first === node){
 		        		eventList[e].first = eventList[e].first.next;
 		        	}
 		        	if(eventList[e].last === node){
@@ -85,7 +90,7 @@
 		        	}
 		        	if(node.next !== null ){
 		        		node.next.previous = node.previous;
-		        	}
+		        	}*/
 		        }
 		        if(null === eventList[e].first){
 		        	delete eventList[e];
@@ -95,18 +100,19 @@
 	};
     EventManager.unbindAll = function(e){
     	if(undefined !== eventList[e]){
-			while(null !== eventList[e].first){
-				var n = eventList[e].first;
-				eventList[e].first = n.next;
-				n.previous = null;
-				n.next = null;
-			}
-			eventList[e].last = null;
-			delete eventList[e];
+    		eventList[e].clear();
+		/*while(null !== eventList[e].first){
+			var n = eventList[e].first;
+			eventList[e].first = n.next;
+			n.previous = null;
+			n.next = null;
 		}
-		return EventManager;
+		eventList[e].last = null;*/
+		delete eventList[e];
+	}
+	return EventManager;
     };
-    EventManager.emit = function(){
+	EventManager.emit = function(){
 		var e = [].shift.apply(arguments);
 		if(undefined === e){console.error("EventManager: You just pass in an event as the first parameter."); return;}
 		if(undefined !== eventList[e]){
@@ -117,7 +123,7 @@
 			}
 		}
 		return EventManager;
-    };
+	};
 	EventManager.cleanup = function(){
 		for(var i in eventList){
 			EventManager.unbindAll(i);
