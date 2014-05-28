c461b9debea7fa8da7959bb684fc28c10233843b
Brett Unzaga <Bunzaga@gmail.com>
2014-05-28 02:59:23 -0600

    Update Test.js

--- Test.js
+++ Test.js
@@ -2,7 +2,7 @@
   var Test = {};
   Test.goo = Scripts.getClasses();
   
-  Test.externals = [key:'Test', name:'Test 123', description:'Just a test', parameters[{key:'int', type:'int', default:0}]];
+  Test.externals = [key:'Test', name:'Test 123', description:'Just a test', parameters:[{key:'int', type:'int', default:0}]];
   
   var global = global || window;
   global.Test = Test;
