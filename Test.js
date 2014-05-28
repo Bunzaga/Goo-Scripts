6b5b85438dea3cea29cb0e09fa685179bfd52804
Brett Unzaga <Bunzaga@gmail.com>
2014-05-28 03:01:22 -0600

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,7 +1,7 @@
 (function(window, document){
   var Test = {};
 
-  Test.externals = [key:'Test', name:'Test 123', description:'Just a test', parameters:[{key:'int', type:'int', default:0}]];
+  Test.externals = {key:'Test', name:'Test 123', description:'Just a test', parameters:[{key:'int', type:'int', default:0}]};
   
   var global = global || window;
   global.Test = Test;
