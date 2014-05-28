0f4b8be813973702b38d7d4ec0fe45259f7b3cf8
Brett Unzaga <Bunzaga@gmail.com>
2014-05-28 03:09:53 -0600

    Delete Test.js

--- Test.js
+++
@@ -1,8 +0,0 @@
-(function(window, document){
-  var Test = {};
-
-  Test.externals = {key:'Test', name:'Test 123', description:'Just a test', parameters:[{key:'int', type:'int', default:0}]};
-  
-  var global = global || window;
-  global.Test = Test;
-}(window, document, undefined));
