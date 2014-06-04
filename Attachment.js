6b642bf80508cd413251927c1d163d943b4c76e4
Brett Unzaga <Bunzaga@gmail.com>
2014-06-04 02:26:42 -0600

    Update Attachment.js

--- Attachment.js
+++ Attachment.js
@@ -8,46 +8,13 @@
         ctx.parent = ctx.parent.entity;
         console.log("ctx.parent");
         console.log(ctx.parent);
-        
-        
-        var a = ctx.world.createEntity(ctx.attachee.name+"_Attachment");
-        
-        a.oldScale = new goo.Vector3().copy(ctx.attachee.transformComponent.transform.scale);
-        ctx.attachee.transformComponent.setScale(1,1,1);
-	ctx.attachee.transformComponent.setUpdated();
-	
-        if(ctx.offsetScl){
-            a.transformComponent.setScale(ctx.offsetScl);
-            console.log("ctx.offsetScl");
-            console.log(ctx.offsetScl);
-        }
-        a.addToWorld();
-        ctx.parent.transformComponent.attachChild(a.transformComponent);
+
+        ctx.parent.transformComponent.attachChild(ctx.attachee);
         ctx.parent.transformComponent.setUpdated();
 
-        a.transformComponent.attachChild(ctx.attachee.transformComponent);
-        a.transformComponent.setUpdated();
-
-        if(ctx.offsetPos){
-            ctx.attachee.transformComponent.setTranslation(
-                ctx.offsetPos[0]*(1/ctx.entity.transformComponent.transform.scale[0]),
-                ctx.offsetPos[1]*(1/ctx.entity.transformComponent.transform.scale[1]),
-                ctx.offsetPos[2]*(1/ctx.entity.transformComponent.transform.scale[2]));
-            console.log("ctx.offsetPos");
-            console.log(ctx.offsetPos);
-        }
- 
-        if(ctx.offsetRot){
-            ctx.attachee.transformComponent.transform.rotation.fromAngles(ctx.offsetRot[0], ctx.offsetRot[1], ctx.offsetRot[2]);
-            console.log("ctx.offsetRot");
-            console.log(ctx.offsetRot);
-        }
-        
         var pose = ctx.parent.animationComponent._skeletonPose;
-        
         ctx.jointTransform = pose._globalTransforms[args.jointIndex];
         
-        ctx.entity.attachment = a;
         console.log("ctx.entity.attachment");
         console.log(ctx.entity.attachment);
   }
@@ -61,12 +28,11 @@
         delete ctx.entity.attachment;
   }
   Attachment.prototype.update = function(args, ctx, goo){
-
-		ctx.entity.attachment.transformComponent.transform.matrix.copy(ctx.jointTransform.matrix);
-		ctx.jointTransform.matrix.getTranslation(ctx.entity.attachment.transformComponent.transform.translation);
-		ctx.jointTransform.matrix.getScale(ctx.entity.attachment.transformComponent.transform.scale);
-		ctx.jointTransform.matrix.getRotation(ctx.entity.attachment.transformComponent.transform.rotation);
-		updateWorldTransform(ctx.entity.attachment.transformComponent);
+		ctx.attachee.transformComponent.transform.matrix.copy(ctx.jointTransform.matrix);
+		ctx.jointTransform.matrix.getTranslation(ctx.attachee.transformComponent.transform.translation);
+		ctx.jointTransform.matrix.getScale(ctx.attachee.transformComponent.transform.scale);
+		ctx.jointTransform.matrix.getRotation(ctx.attachee.transformComponent.transform.rotation);
+		updateWorldTransform(ctx.attachee.transformComponent);
 		ctx.entity.attachment.transformComponent._dirty = true;
   }
 	function updateWorldTransform(transformComponent) {
