3e652ed7e3ff301401a943cd10f0f5ef35d9a049
Brett Unzaga <Bunzaga@gmail.com>
2014-06-19 10:40:13 -0600

    Update VehicleHelper.js

--- VehicleHelper.js
+++ VehicleHelper.js
@@ -70,6 +70,7 @@
   	wheel.set_m_rollInfluence(0); // this value controls how easily a vehicle can tipp over. Lower values tipp less :)
   };
   VehicleHelper.prototype.updateWheelTransform = function(dt){
+    var dt = Number(dt);
     var vs = this.vehicle.getSteeringValue();
     this.entity.transformComponent.transform.rotation.toAngles(this.vec);
     var ef = this.vehicle.getCurrentSpeedKmHour();
@@ -79,7 +80,7 @@
   		this.vehicle.updateWheelTransform(i, true);
   		var dt = this.debugTires[i];
   		if(dt) {
-  		  var r = this.vehicle.getWheelInfo(i).get_m_rotation()*dt;
+  		  var r = this.vehicle.getWheelInfo(i).get_m_rotation() * dt;
   		  console.log(r);
   		  
         if(i < 2){
