5adaae000675dd9f4f5422a2ed140e125fac8cbb
Brett Unzaga <Bunzaga@gmail.com>
2014-07-27 17:20:59 -0600

    Update AmmoUtil.js

--- AmmoUtil.js
+++ AmmoUtil.js
@@ -99,6 +99,7 @@
   			col = AmmoUtil.createBoxColliderComponent({halfExtents:[md.xExtent * scl[0], md.yExtent * scl[1], 0.01]}, goo);
   		}else if(md instanceof goo.Cylinder){
   			col = AmmoUtil.createCylinderZColliderComponent({radius:md.radius * *scl[0], height:md.yExtent * scl[1] * 2}, goo);
+  			console.log(md.yExtent);
   		}
   	}
   	else{
