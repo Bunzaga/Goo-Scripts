970044ae6764076326eebe0eaaa1b67c505d2575
Brett Unzaga <Bunzaga@gmail.com>
2014-08-06 04:52:18 -0600

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,4 +1,10 @@
-require(['goo'], function(goo){
+require.config({
+      paths: {
+          'goo.js': 'http://code.gooengine.com/0.10.8/lib/goo.js'
+      }
+  });
+
+require(['goo.js'], function(goo){
   console.log(goo);
   function Test(){
     this.a = new goo.Vector3();
