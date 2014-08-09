0ecd69e2f69eef985ca0fb919ea0f381f9ec671a
Brett Unzaga <Bunzaga@gmail.com>
2014-08-09 05:30:21 -0600

    Update AmmoUtil.js

--- AmmoUtil.js
+++ AmmoUtil.js
@@ -158,14 +158,13 @@
   		var startTransform = new Ammo.btTransform();
   		startTransform.setIdentity();
 		var gooPos = ctx.entity.transformComponent.transform.translation;
-		this.oldPos.copy(gooPos);
 		if(collider.offset){
 			gooVec = gooVec || new goo.Vector3();
 			gooVec.copy(collider.offset);
 			ctx.entity.transformComponent.transform.rotation.applyPost(gooVec);
-			//ctx.entity.transformComponent.transform.applyForwardVector(collider.offset, gooVec);
 			gooPos.subv(gooVec);
 		}
+		this.oldPos.copy(gooPos);
 		var gooRot = ctx.entity.transformComponent.transform.rotation;
 		var localInertia = new Ammo.btVector3(0, 0, 0);
 		if(this.mass !== 0){
