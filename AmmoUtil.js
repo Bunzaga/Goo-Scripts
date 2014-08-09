32f2113562913f651eb658dd0f352eb83ae937ce
Brett Unzaga <Bunzaga@gmail.com>
2014-08-09 03:24:57 -0600

    Update AmmoUtil.js

--- AmmoUtil.js
+++ AmmoUtil.js
@@ -157,13 +157,13 @@
   		var startTransform = new Ammo.btTransform();
   		startTransform.setIdentity();
 		var gooPos = ctx.entity.transformComponent.transform.translation;
+		this.oldPos.copy(gooPos);
 		if(collider.offset){
 			gooVec = gooVec || new goo.Vector3();
 			gooVec.copy(collider.offset);
 			ctx.entity.transformComponent.transform.applyForwardVector(collider.offset, gooVec);
 			gooPos.subv(gooVec);
 		}
-		this.oldPos.copy(gooPos);
 		var gooRot = ctx.entity.transformComponent.transform.rotation;
 		var localInertia = new Ammo.btVector3(0, 0, 0);
 		if(this.mass !== 0){
@@ -188,8 +188,8 @@
  		quat = quat || new goo.Quaternion();
  		gooVec = gooVec || new goo.Vector3();
  		
- 		ptrans = this.body.getCenterOfMassTransform();
- 		//this.body.getMotionState().getWorldTransform(ptrans);
+ 		//ptrans = this.body.getCenterOfMassTransform();
+ 		this.body.getMotionState().getWorldTransform(ptrans);
   		ptrans.getBasis().getRotation(pquat);
 		this.oldQuat.setd(pquat.x(), pquat.y(), pquat.z(), pquat.w());
 		
@@ -208,8 +208,8 @@
  		quat = quat || new goo.Quaternion();
  		gooVec = gooVec || new goo.Vector3();
  		
- 		ptrans = this.body.getCenterOfMassTransform();
-  		//this.body.getMotionState().getWorldTransform(ptrans);
+ 		//ptrans = this.body.getCenterOfMassTransform();
+  		this.body.getMotionState().getWorldTransform(ptrans);
   		ptrans.getBasis().getRotation(pquat);
 		quat.setd(
 			(this.oldQuat.x * negAlpha) + (pquat.x() * alpha),
