5fdf50fb3ccb1c62ad8e75589162734895be1e2d
Brett Unzaga <Bunzaga@gmail.com>
2014-08-08 22:52:15 -0600

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,11 +1,13 @@
-define('Test', ['goo/math/Vector3'], function(Vector3){
-  var Test = {};
-  var aVec = new Vector3(1,2,3);
-
-  Test.printStuff = function(){
-    console.log(aVec);
-  }
-  var global = global || window;
-  global.Test = Test;
-  return Test;
-});
+(function(window, document, undefined){
+  define('Test', ['goo/math/Vector3'], function(Vector3){
+    var Test = {};
+    var aVec = new Vector3(1,2,3);
+  
+    Test.printStuff = function(){
+      console.log(aVec);
+    }
+    var global = global || window;
+    global.Test = Test;
+    return Test;
+  });
+}(window, document));
