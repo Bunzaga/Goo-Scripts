859f475cb5a0ef49c2c4f89331d2f6c9c9b75628
Brett Unzaga <Bunzaga@gmail.com>
2014-08-13 15:25:27 -0600

    Update Attachment.js

--- Attachment.js
+++ Attachment.js
@@ -7,7 +7,7 @@
         //console.log("ctx.parent");
         //console.log(ctx.parent);
 
-        ctx.parent.transformComponent.attachChild(ctx.attachee.transformComponent);
+        ctx.parent.transformComponent.attachChild(ctx.attachee.transformComponent, true);
         ctx.parent.transformComponent.setUpdated();
 
         var pose = ctx.parent.animationComponent._skeletonPose;
