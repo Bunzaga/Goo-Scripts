50e6d7d7d85abb07a037c917196f5aca3ff43f21
Brett Unzaga <Bunzaga@gmail.com>
2014-08-13 16:47:10 -0600

    Update Attachment.js

--- Attachment.js
+++ Attachment.js
@@ -15,7 +15,7 @@
         	args.attachee.jointTransform = pose._globalTransforms[args.jointIndex];
         	args.attachee.offsetScale = new goo.Vector3().copy(parent.transformComponent.transform.scale);
         	args.attachee.offsetScale.mulv(args.attachee.transformComponent.transform.scale);
-        	//parent.transformComponent.attachChild(args.attachee.transformComponent);
+        	parent.transformComponent.attachChild(args.attachee.transformComponent);
         }
         console.log(args.attachee.offsetScale);
   }
@@ -28,8 +28,7 @@
 		args.attachee.jointTransform.matrix.getScale(args.attachee.transformComponent.transform.scale);
 		args.attachee.jointTransform.matrix.getRotation(args.attachee.transformComponent.transform.rotation);
 		Attachment.updateWorldTransform(args.attachee.transformComponent);
-		args.attachee.transformComponent.transform.scale.mulv(args.attachee.offsetScale);
-		args.attachee.transformComponent.transform.translation.mulv(args.attachee.transformComponent.transform.scale);
+		args.attachee.transformComponent.transform.scale.div(args.attachee.offsetScale);
 		args.attachee.transformComponent._dirty = true;
 	}
 	Attachment.updateWorldTransform = function(transformComponent){
