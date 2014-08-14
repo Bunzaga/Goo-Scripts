7b32e09a400c0633924b91e18c479274c618aaed
Brett Unzaga <Bunzaga@gmail.com>
2014-08-14 04:35:09 -0600

    Delete Test.js

--- Test.js
+++
@@ -1,9 +0,0 @@
-define('Test', ['goo/math/Vector3'], function(Vector3){
-  var Test = {};
-  var aVec = new Vector3(1,2,3);
-
-  Test.printStuff = function(){
-    console.log(aVec);
-  }
-  return Test;
-});
