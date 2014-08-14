1b32d7dcf052ab4608b996c6f842da226ef4c727
Brett Unzaga <Bunzaga@gmail.com>
2014-08-14 06:04:21 -0600

    Update AmmoUtil.js

--- AmmoUtil.js
+++ AmmoUtil.js
@@ -226,10 +226,14 @@ AmmoUtil.createAmmoSystem = function(args){
   	RigidBodyComponent.prototype.setTranslation = function(vec3){
   		pvec.setValue(vec3[0], vec3[1], vec3[2]);
 		//var trans = this.body.getWorldTransform();
-		this.body.getMotionState().getWorldTransform(trans);
-		trans.setOrigin(pvec);
-		this.body.getMotionState().setWorldTransform(trans);
-		this.body.setCenterOfMassTransform(trans);
+		//trans.setOrigin(pvec);
+		//this.body.getMotionState().setWorldTransform(trans);
+		//this.body.setCenterOfMassTransform(trans);
+		
+		this.body.getMotionState().getWorldTransform(ptrans);
+		ptrans.setOrigin(pvec);
+		this.body.getMotionState().setWorldTransform(ptrans);
+		this.body.setCenterOfMassTransform(ptrans);
   	};
   	RigidBodyComponent.prototype.setRotation = function(mat3x3){
   		quat.fromRotationMatrix(mat3x3);
