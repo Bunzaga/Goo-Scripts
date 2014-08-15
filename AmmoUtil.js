62281e133f6ae2567961d4375036db0728c4faf1
Brett Unzaga <Bunzaga@gmail.com>
2014-08-15 11:44:01 -0600

    Update AmmoUtil.js

--- AmmoUtil.js
+++ AmmoUtil.js
@@ -86,7 +86,7 @@ AmmoUtil.createAmmoSystem = function(args){
 	  		if(ent.rigidBodyComponent){
 				var body = AmmoUtil.rigidBodies[ent.rigidBodyComponent.ptr];
 				delete AmmoUtil.rigidBodies[ent.rigidBodyComponent.ptr];
-				this.ammoWorld.removeCollisionObject(body);
+				ammoSystem.ammoWorld.removeCollisionObject(body);
 				if(body.getMotionState()){
 					Ammo.destroy(body.getMotionState());
 				}
