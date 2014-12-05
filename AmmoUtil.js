4aa3942485fec0bd152fd1a345255e82c7e22f41
Brett Unzaga <Bunzaga@gmail.com>
2014-12-05 03:50:43 -0700

    Update AmmoUtil.js

--- AmmoUtil.js
+++ AmmoUtil.js
@@ -251,11 +251,16 @@ AmmoUtil.createAmmoSystem = function(args){
   	//	Math.abs(ent.transformComponent.transform.scale[2])];
   		
   	function setScale(e1){
+  		console.log(e1);
+  		console.log('Before');
+  		console.log(scl[0]+","+scl[1]+","+scl[2]);
 	//	if(e1 !== ent){
 			scl[0] *= Math.abs(e1.transformComponent.transform.scale[0]);
 			scl[1] *= Math.abs(e1.transformComponent.transform.scale[1]);
 			scl[2] *= Math.abs(e1.transformComponent.transform.scale[2]);
 	//	}
+		console.log('After');
+		console.log(scl[0]+","+scl[1]+","+scl[2]);
 	}
 	//fix scaleing issues for all parents
 	ent.traverseUp(setScale);
