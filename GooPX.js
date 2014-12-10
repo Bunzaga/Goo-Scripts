ffb3c73b17f32b9c8e6b3b25abfe3eafab75686f
Brett Unzaga <Bunzaga@gmail.com>
2014-12-10 12:11:56 -0700

    Update GooPX.js

--- GooPX.js
+++ GooPX.js
@@ -1,37 +1,38 @@
 (function(window, undefined){
-  'use strict';
-  var GooPX = {};
-  GooPX.System = function(settings){
-    goo.System.call(this, 'GooPX.System', ['GooPX.RigidbodyComponent']);
-    settings = settings || {};
-    _.defaults(settings, {
-      gravity:goo.Vector3(0, -9.8, 0)
+	'use strict';
+	var GooPX = {};
+	GooPX.System = function(settings){
+		goo.System.call(this, 'GooPX.System', ['GooPX.RigidbodyComponent']);
+		settings = settings || {};
+		_.defaults(settings, {
+			gravity:goo.Vector3(0, -9.8, 0)
 		});
-    this.inserted = function(){
-    	console.log('GooPX.System.inserted()');
-    }
-    this.deleted = function(){
-    	console.log('GooPX.System.deleted()');
-    }
-    this.process = function(){
-    	console.log('GooPX.System.process()');
-    }
-    this.cleanup = function(){
-    	console.log('GooPX.System.cleanup()');
-    }
-  }
-  GooPX.System.prototype = Object.create(goo.System.prototype);
-  GooPX.System.constructor = GooPX.System;
+		console.log('GooPX.System constructor');
+	};
+	GooPX.System.prototype.inserted = function(){
+	    	console.log('GooPX.System.inserted()');
+	};
+	GooPX.System.prototype.deleted = function(){
+		console.log('GooPX.System.deleted()');
+	};
+	GooPX.System.prototype.process = function(){
+		console.log('GooPX.System.process()');
+	};
+	GooPX.System.prototype.cleanup = function(){
+		console.log('GooPX.System.cleanup()');
+	};
+	GooPX.System.prototype = Object.create(goo.System.prototype);
+	GooPX.System.constructor = GooPX.System;
   
-  GooPX.RigidbodyComponent = function(settings){
-    settings = settings || {};
-    _.defaults(settings,{
-      mass:1.0
-    });
-    this.type = 'GooPX.RigidbodyComponent';
-  }
-  GooPX.RigidbodyComponent.prototype = Object.create(goo.Component.prototype);
-  GooPX.RigidbodyComponent.constructor = GooPX.RigidbodyComponent;
-  var global = global || window;
-  window.GooPX = GooPX;
+	GooPX.RigidbodyComponent = function(settings){
+		settings = settings || {};
+		_.defaults(settings,{
+			mass:1.0
+		});
+		this.type = 'GooPX.RigidbodyComponent';
+	};
+	GooPX.RigidbodyComponent.prototype = Object.create(goo.Component.prototype);
+	GooPX.RigidbodyComponent.constructor = GooPX.RigidbodyComponent;
+	var global = global || window;
+	window.GooPX = GooPX;
 }(window));
