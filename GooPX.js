37dabe6d89ed00558c6e842aaf15a62f41b85146
Brett Unzaga <Bunzaga@gmail.com>
2014-12-14 22:10:17 -0700

    Update GooPX.js

--- GooPX.js
+++ GooPX.js
@@ -208,10 +208,7 @@
 	GooPX.checkCollision = function(entA, entB){
 		console.log('GooPX.checkCollision()');
 		console.log(entA.name+":"+entB.name);
-		var sf = entA.colliderComponent.collider.type+"_"+entB.colliderComponent.collider.type+"Support";
-		console.log(sf);
-		var result = GooPX[sf](entA, entB);
-		return result;
+		return GooPX[entA.colliderComponent.collider.type+"_"+entB.colliderComponent.collider.type+"Support"](entA, entB);
 	};
 	GooPX.BoxCollider = function(){};
 	GooPX.BoxCollider.pool = [];
