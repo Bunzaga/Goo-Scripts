d230bc954a00686d9ff3427a2376659329037391
Brett Unzaga <Bunzaga@gmail.com>
2015-01-13 15:00:26 -0700

    Update GooPX.js

--- GooPX.js
+++ GooPX.js
@@ -1,21 +1,35 @@
 (function(window, document, undefined){
-var GooPX = {};
-  GooPX.CannonSystem = function(settings){
-    goo.System.call(this, 'GooPXSystem', ['RigidbodyComponent', 'ColliderComponent']);
-    this.priority = 1;
-    this.gravity = settings === undefined || settings.gravity === undefined ? new goo.Vector3() : settings.gravity;
-    this.stepFrequency = settings === undefined || settings.stepFrequency === undefined ? 60 : settings.stepFrequency;
-    this.broadphase = settings === undefined || settings.broadphase === undefined ? 'naive' : settings.broadphase;
-    this.maxSubSteps = settings === undefined || settings.maxSubSteps === undefined ? 0 : settings.maxSubSteps;
+	var GooPX = {};
+	GooPX.CannonSystem = function(settings){
+		goo.System.call(this, 'GooPXSystem', ['RigidbodyComponent', 'ColliderComponent']);
+		this.priority = 1;
+		this.gravity = settings === undefined || settings.gravity === undefined ? new goo.Vector3() : settings.gravity;
+		this.stepFrequency = settings === undefined || settings.stepFrequency === undefined ? 60 : settings.stepFrequency;
+		this.broadphase = settings === undefined || settings.broadphase === undefined ? 'naive' : settings.broadphase;
+		this.maxSubSteps = settings === undefined || settings.maxSubSteps === undefined ? 0 : settings.maxSubSteps;
     
-    var world = this.world = new CANNON.World();
+		var world = this.world = new CANNON.World();
 		world.gravity.x = this.gravity.x;
 		world.gravity.y = this.gravity.y;
 		world.gravity.z = this.gravity.z;
 		this.setBroadphaseAlgorithm(this.broadphase);
-  };
-  GooPX.CannonSystem.prototype = Object.create(goo.System.prototype);
-  GooPX.CannonSystem.prototype.constructor = GooPX.CannonSystem;
+  	};
+	GooPX.CannonSystem.prototype = Object.create(goo.System.prototype);
+	GooPX.CannonSystem.prototype.constructor = GooPX.CannonSystem;
+  
+	GooPX.CannonSystem.prototype.setBroadphaseAlgorithm = function(algorithm){
+		var world = this.world;
+		switch(algorithm){
+		case 'naive':
+			world.broadphase = new CANNON.NaiveBroadphase();
+			break;
+		case 'sap':
+			world.broadphase = new CANNON.SAPBroadphase(world);
+			break;
+		default:
+			throw new Error('Broadphase not supported: ' + algorithm);
+		}
+	};
   
   GooPX.RigidbodyComponent = function(){
     goo.Component.call(this, arguments);
