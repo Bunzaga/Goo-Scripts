1e620133044c23a9c74343c063425daa0cf0cc0b
Brett Unzaga <Bunzaga@gmail.com>
2015-01-13 18:35:39 -0700

    Update GooPX.js

--- GooPX.js
+++ GooPX.js
@@ -14,7 +14,7 @@
 	GooPX.CannonSystem = function(settings){
 		goo.System.call(this, 'GooPXSystem', ['RigidbodyComponent', 'ColliderComponent']);
 		this.priority = 1;
-		this.gravity = settings === undefined || settings.gravity === undefined ? new goo.Vector3() : settings.gravity;
+		this.gravity = settings === undefined || settings.gravity === undefined ? new goo.Vector3(0, -9.806, 0) : settings.gravity;
 		this.stepFrequency = settings === undefined || settings.stepFrequency === undefined ? 60 : settings.stepFrequency;
 		this.broadphase = settings === undefined || settings.broadphase === undefined ? 'naive' : settings.broadphase;
 		this.maxSubSteps = settings === undefined || settings.maxSubSteps === undefined ? 0 : settings.maxSubSteps;
